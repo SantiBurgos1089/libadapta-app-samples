b8b7515e41cf7c788eaf7d214d659f4fe07ab3aa
Santiago Burgos <santiago.burgos1089@gmail.com>
2026-03-02 15:22:09 -0600

    added general and background sections along with their controls

--- app_sample7/sample7_appearance.py
+++ app_sample7/sample7_appearance.py
@@ -20,9 +20,11 @@ class DemoAppearance(Gtk.Box):
         self.general_section = Adw.PreferencesPage()
         self.general_section.set_title("General")
 
+        # Seccion General
         self.general_group = Adw.PreferencesGroup()
         self.general_group.set_title("General")
 
+        # Alignment row
         self.general_row = Adw.ActionRow()
         self.general_row.set_title("Alignment")
 
@@ -34,5 +36,55 @@ class DemoAppearance(Gtk.Box):
         self.general_group.add(self.general_row)
 
         self.general_section.add(self.general_group)
+
+        # Seccion Background
+        self.background_group = Adw.PreferencesGroup()
+        self.background_group.set_title("Background")
+
+        # Background row
+        self.background_row = Adw.ActionRow()
+        self.background_row.set_title("Background")
+
+        self.background_entry = Gtk.Entry()
+        self.background_entry.set_hexpand(True)
+        self.background_entry.set_property("editable", False)
+        self.background_row.add_suffix(self.background_entry)
+
+        self.background_group.add(self.background_row)
+
+        # Background color row
+        self.bgcolor_row = Adw.ActionRow()
+        self.bgcolor_row.set_title("Background color")
+
+        self.color_dialog = Gtk.ColorDialog()
+        self.color_picker = Gtk.ColorDialogButton.new(dialog=self.color_dialog)
+        self.bgcolor_row.add_suffix(self.color_picker)
+
+        self.background_group.add(self.bgcolor_row)
+
+        # Stretch background row
+        self.stretchbg_switch = Adw.SwitchRow()
+        self.stretchbg_switch.set_title("Stretch background across multiple monitors")
+        self.stretchbg_switch.set_active(False)
+
+        self.background_group.add(self.stretchbg_switch)
+
+        # Draw user background row
+        self.userbg_switch = Adw.SwitchRow()
+        self.userbg_switch.set_title("Draw user backgrounds")
+        self.userbg_switch.set_subtitle("When a user is selected, show that user's background.")
+        self.userbg_switch.set_active(False)
+
+        self.background_group.add(self.userbg_switch)
+
+        # Draw a grid row
+        self.gridbg_switch = Adw.SwitchRow()
+        self.gridbg_switch.set_title("Draw a grid")
+        self.gridbg_switch.set_subtitle("Draw a grid of white dots on top of the background.")
+        self.gridbg_switch.set_active(False)
+
+        self.background_group.add(self.gridbg_switch)
+
+        self.general_section.add(self.background_group)
 
         self.append(self.general_section)
