403df70d8ea19a79db5e822e20e6c9b3b474db59
Santiago Burgos <santiago.burgos1089@gmail.com>
2026-03-03 11:12:14 -0600

    deleted Glib dependency from all sample pages, since for now it's not being used on the examples

--- app_sample7/sample7_users.py
+++ app_sample7/sample7_users.py
@@ -2,7 +2,7 @@
 
 import gi
 gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk, GLib
+from gi.repository import Gtk
 
 gi.require_version('Adap', '1')
 from gi.repository import Adap as Adw
