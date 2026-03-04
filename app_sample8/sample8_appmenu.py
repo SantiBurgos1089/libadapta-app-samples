0fc7186b7241348680d7e2698859bac595b9d2c6
Santiago Burgos <santiago.burgos1089@gmail.com>
2026-03-04 15:57:54 -0600

    added a comment on how the sample appmenu works visually

--- app_sample8/sample8_appmenu.py
+++ app_sample8/sample8_appmenu.py
@@ -4,11 +4,13 @@ import gi
 gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk, Gio
 
+# This will show the menus needed, however, they won't be clickable due to not defining actions
+# on them, if you wish to add those options, you can follow the instructions given here:
+# https://github.com/Taiko2k/GTK4PythonTutorial?tab=readme-ov-file#adding-a-button-with-menu
+# The idea is to show a visual menu but not implement actions on them
 class DemoMenu(Gtk.MenuButton):
      def __init__(self, **kwargs):
           super().__init__()
-
-          self.set_icon_name("xsi-open-menu-symbolic")
 
           demo_menu = Gio.Menu()
 
@@ -22,4 +24,6 @@ class DemoMenu(Gtk.MenuButton):
           demo_menu.append_section(None, section2)
 
           demo_popover = Gtk.PopoverMenu.new_from_model(demo_menu)
+
+          self.set_icon_name("xsi-open-menu-symbolic")
           self.set_popover(demo_popover)
